d688f4af2b82624b9c8f698451d5165980845615
Ned Schwartz <ned@theinterned.net>
2016-03-22 15:33:40 -0400

    renamed the callback in the addContact action to make it less specific about its use

--- js/actions/contactActions.js
+++ js/actions/contactActions.js
@@ -2,10 +2,10 @@ import { ADD_CONTACT } from '../constants/AppConstants';
 
 let nextContactID = 3; // see contactReducer.js for ids 1 and 2
 
-export function addContact(contact, redirect) {
+export function addContact(contact, callback) {
   const nextID = nextContactID++;
-  if (redirect) {
-    redirect(nextID, contact);
+  if (callback) {
+    callback(nextID, contact);
   }
   return {
     type: ADD_CONTACT,
